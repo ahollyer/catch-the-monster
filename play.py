107db24eae8edfd30172a9d3c75578406cf724f1
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-27 13:33:09 -0500

    Complete step 11

--- play.py
+++ play.py
@@ -1,17 +1,12 @@
 import pygame
 import random
 
+from toons.monster import Monster
+
 def main():
     width = 512
     height = 480
     blue_color = (97, 159, 182)
-
-    # sprite postitioning/movement
-    x = 0
-    y = 1
-    speed_x = 3
-    speed_y = -3
-    change_dir_countdown = 60
 
     pygame.init()
     screen = pygame.display.set_mode((width, height))
@@ -19,11 +14,12 @@ def main():
     clock = pygame.time.Clock()
 
     # Game initialization
+    monster = Monster(width=width, height=height)
+
     background_image = pygame.image.load(
         'images/background.png').convert_alpha()
-    hero = pygame.image.load('images/hero.png')
-    monster = pygame.image.load('images/monster.png')
-    monster_pos = [30, 25]
+    hero_sprite = pygame.image.load('images/hero.png')
+    monster_sprite = pygame.image.load('images/monster.png')
 
     stop_game = False
     while not stop_game:
@@ -36,35 +32,23 @@ def main():
 
 
         # Game logic
-        def wrap_monster():
-            if monster_pos[x] > width:
-                monster_pos[x] = 0
-            elif monster_pos[x] < 0:
-                monster_pos[x] = width
-            elif monster_pos[y] > height:
-                monster_pos[y] = 0
-            elif monster_pos[y] < 0:
-                monster_pos[y] = height
 
-        wrap_monster()
-        monster_pos[x] += speed_x
-        monster_pos[y] += speed_y
+        monster.change_dir_countdown -= 1
+        if monster.change_dir_countdown == 0:
+            monster.speed_x = random.randint(-5, 5)
+            monster.speed_y = random.randint(-5, 5)
+            monster.change_dir_countdown = 60
 
-        change_dir_countdown -= 1
-        if change_dir_countdown == 0:
-            speed_x = random.randint(-5, 5)
-            speed_y = random.randint(-5, 5)
-            change_dir_countdown = 60
-
+        monster.wrap()
+        monster.move()
 
         # Draw background
         screen.fill(blue_color)
 
         # Game display
         screen.blit(background_image, (0, 0))
-        screen.blit(hero, (250, 240))
-        print(monster_pos)
-        screen.blit(monster, (monster_pos[x], monster_pos[y]))
+        screen.blit(hero_sprite, (250, 240))
+        screen.blit(monster_sprite, (monster.pos[0], monster.pos[1]))
         pygame.display.update()
         clock.tick(60)
 
---
+++ toons/monster.py
@@ -0,0 +1,23 @@
+class Monster:
+        def __init__(self, pos=[30, 25], speed_x=3, speed_y=-3, change_dir_countdown=60, width=0, height=0):
+            self.pos = pos
+            self.speed_x = speed_x
+            self.speed_y = speed_y
+            self.change_dir_countdown = change_dir_countdown
+            self.width = width
+            self.height = height
+
+        def wrap(self):
+            if self.pos[0] > self.width:
+                self.pos[0] = 0
+            elif self.pos[0] < 0:
+                self.pos[0] = self.width
+            elif self.pos[1] > self.height:
+                self.pos[1] = 0
+            elif self.pos[1] < 0:
+                self.pos[1] = self.height
+
+        def move(self):
+            self.pos[0] += self.speed_x
+            self.pos[1] += self.speed_y
+            return [self.pos[0], self.pos[1]]
